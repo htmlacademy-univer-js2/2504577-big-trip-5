4523b845a7b8610a33d094c2cff1ac3b9a4db7a8
Krutyash <kostrov2525@gmail.com>
2025-02-17 11:37:28 +0500

    Время разбивать камни

---
+++ src/view/trip-filters-view.js
@@ -0,0 +1,35 @@
+import { createElement } from "../render";
+
+const TYPES_FILTER = ["everything", "future", "present", "past"]
+
+function createFiltersItemTemplate(filter, checked=false) {
+  return (`<div class="trip-filters__filter">
+            <input id="filter-${filter}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter}" ${checked ? "checked" : ""}>
+            <label class="trip-filters__filter-label" for="filter-${filter}">${filter}</label>
+          </div>`);
+}
+
+function createTripFiltersTemplate() {
+  return (`<form class="trip-filters" action="#" method="get">
+            ${TYPES_FILTER.map((filter) => (filter === "everything" ? createFiltersItemTemplate(filter, true) : createFiltersItemTemplate(filter))).join("")}
+            <button class="visually-hidden" type="submit">Accept filter</button>
+          </form>`);
+}
+
+export default class TripFiltersView {
+  getTemplate() {
+    return createTripFiltersTemplate();
+  }
+
+  getElement() {
+    if (!this.element){
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
---
+++ src/view/trip-sort-view.js
@@ -0,0 +1,36 @@
+import { createElement } from "../render";
+
+const TYPES_SORT = ["day", "event", "time", "price", "offers"]
+
+function createSortItemTemplate(sort, checked=false, disabled=false) {
+  return (`<div class="trip-sort__item  trip-sort__item--${sort}">
+            <input id="sort-${sort}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sort}"
+            ${checked ? "checked" : ""} ${disabled ? "disabled" : ""}>
+            <label class="trip-sort__btn" for="sort-${sort}">${sort}</label>
+          </div>`);
+}
+
+function createSortTemplate() {
+  return (`<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+            ${TYPES_SORT.map((sort) => (["event", "offers"].includes(sort) ? createSortItemTemplate(sort, false, true) : sort === "day" ?
+            createSortItemTemplate(sort, true) : createSortItemTemplate(sort))).join("")}
+          </form>`);
+}
+
+export default class TripSortView {
+  getTemplate() {
+    return createSortTemplate();
+  }
+
+  getElement() {
+    if (!this.element){
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
